576d2b88fefc59b75f418e2d83d5fe61e531f308
WanjikuSharon <sharonwanjiku292@gmail.com>
2024-11-16 19:13:37 +0300

    About

--- customers/urls.py
+++ customers/urls.py
@@ -21,5 +21,6 @@ from customers import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index/',views.index,name='index')
+    path('index/',views.index,name='index'),
+    path('about/',views.about,name='about')
 ]
