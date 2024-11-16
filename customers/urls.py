f6c88374bbf6217486f615a215851091a58dcdfe
WanjikuSharon <sharonwanjiku292@gmail.com>
2024-11-16 19:18:15 +0300

    Adding page to application url.py

--- customers/urls.py
+++ customers/urls.py
@@ -22,5 +22,6 @@ from customers import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('index/',views.index,name='index'),
-    path('about/',views.about,name='about')
+    path('about/',views.about,name='about'),
+    path('contact/',views.contact,name='contact')
 ]
