6d56a17fae0ca89f72c0adb8b43be2eb780c6b6a
WanjikuSharon <sharonwanjiku292@gmail.com>
2024-11-19 19:22:45 +0300

    Admission number

--- customers/models.py
+++ customers/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Customer(models.Model):
     name=models.CharField(max_length=20)
+    admission_number=models.CharField(max_length=10)
     email=models.EmailField(max_length=40)
     gender=models.CharField(max_length=20)
     age=models.IntegerField()
