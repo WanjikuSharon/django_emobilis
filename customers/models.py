97ae7742c1f8db83b901622f943377ffa5ef45d0
WanjikuSharon <sharonwanjiku292@gmail.com>
2024-11-19 15:45:52 +0300

    Model forms

--- customers/models.py
+++ customers/models.py
@@ -2,4 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Customers(models.Model):
-    name=models.CharField(max_length=20),
+    name=models.CharField(max_length=20)
+    email=models.EmailField(max_length=25)
+    gender=models.CharField(max_length=20)
+    age=models.IntegerField()
+
+def __str__(self):
+    return self.name
