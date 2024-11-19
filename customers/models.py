835d71c2d772447cde69a0c5a7b9b4d10b4c0786
WanjikuSharon <sharonwanjiku292@gmail.com>
2024-11-19 18:33:28 +0300

    Error

--- customers/models.py
+++ customers/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Customers(models.Model):
     name=models.CharField(max_length=20)
-    email=models.EmailField(max_length=25)
+    email=models.EmailField(max_length=40)
     gender=models.CharField(max_length=20)
     age=models.IntegerField()
 
