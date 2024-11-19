837be1da6c1d9ffe509b799d7d049206683f0b4e
WanjikuSharon <sharonwanjiku292@gmail.com>
2024-11-19 17:31:03 +0300

    Form control

--- customers/forms.py
+++ customers/forms.py
@@ -7,3 +7,10 @@ class CustomerForm(forms.ModelForm):
     class Meta:
         model = Customers
         fields = '__all__'
+
+        widgets= {
+            'name':forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter Your name'}),
+            'email': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter Your email'}),
+            'gender': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter Your gender'}),
+            'age': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter Your age'}),
+        }
