6ad2334ddfa4b8388e6809e25f0c813608dd6a01
WanjikuSharon <sharonwanjiku292@gmail.com>
2024-11-20 15:47:29 +0300

    Pillow

--- customers/models.py
+++ customers/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Customer(models.Model):
+    image = models.ImageField(upload_to='customer_images/', blank=True)
     name=models.CharField(max_length=20)
     admission_number=models.CharField(max_length=10)
     email=models.EmailField(max_length=40)
