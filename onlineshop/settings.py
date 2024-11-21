1a89f055491e0643f36bb9297b07078a69a560ce
WanjikuSharon <sharonwanjiku292@gmail.com>
2024-11-21 17:49:45 +0300

    Changing DB

--- onlineshop/settings.py
+++ onlineshop/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'onlineshop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'online_agricultural_shop',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
