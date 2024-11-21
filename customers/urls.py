612a67a07d7ba9e814f9650897f30fdc88adda30
WanjikuSharon <sharonwanjiku292@gmail.com>
2024-11-21 12:24:10 +0300

    Should have images from backend

--- customers/urls.py
+++ customers/urls.py
@@ -26,4 +26,4 @@ urlpatterns = [
     path('index/',views.index,name='index'),
     path('about/',views.about,name='about'),
     path('contact/',views.contact,name='contact')
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
