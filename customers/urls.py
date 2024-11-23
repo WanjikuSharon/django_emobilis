5ab6f27bc813cf1db98e633cee6743cc4706e02b
WanjikuSharon <sharonwanjiku292@gmail.com>
2024-11-23 18:13:49 +0300

    Hey

--- customers/urls.py
+++ customers/urls.py
@@ -29,6 +29,6 @@ urlpatterns = [
     path('update/<int:id>/', views.update,name='update'),
     path('delete/<int:id>/', views.delete,name='delete'),
     path('customersapi/', views.customersapi,name='customersapi'),
-    path('orders/', views.orders,name='ordersapi')
+    path('orders/', views.orders,name='ordersapi'),
     path('mpesaapi/', views.mpesaapi,name='mpesaapi')
 ]
