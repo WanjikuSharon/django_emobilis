ef457d8585c47431017300c1174f33f084a10f31
WanjikuSharon <sharonwanjiku292@gmail.com>
2024-11-23 16:21:20 +0300

    Order

--- customers/Serializers.py
+++ customers/Serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from customers.models import Customer
+from customers.models import Customer, Order
+
 
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
         fields = ['name', 'email', 'gender', 'age']
+
+    class Meta:
+        model = Order
+        fields = ['name', 'price', 'quantity']
